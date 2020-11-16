7fbc87abc498fba589559abfa8b1c035ecf0805c
javi <javi@email.com>
2020-11-16 18:53:23 +0100

    before removing fft

--- matlab/freq_analysis.m
+++ matlab/freq_analysis.m
@@ -2,16 +2,20 @@ close all
 clear
 
 %-- signal --
-T = readtable('../meas/step_current_2/NewFile2.csv', 'HeaderLines',2);
-start=163000;
-finish=360000;
+T = readtable('../meas/step_current_3/pwm_1100_short.csv', 'HeaderLines',2);
+start=137146;
+finish=524000;
 t = T{:,1};
 t=t(start:finish);
-t = downsample(t,100)
-v = T{:,3}/10;
+v = T{:,3};
 v=v(start:finish);
-v = downsample(v,100)
 mA = v/0.05;
+
+%-- downsampled --
+period=2489;
+first=850;
+mA_down=mA(first:period:end);
+t_down=t(first:period:end);
 
 %-- common --
 Ts = t(2)-t(1)
@@ -36,16 +40,22 @@ mA_filtered_iir = filter(b,a,mA);
 %-- plots --
 figure
 
-subplot(1,2,1)
+% subplot(1,2,1)
 hold on
 plot(t,mA)
 title('mA')
-plot(t,mA_filtered)
-title('mA filtered')
-plot(t,mA_filtered_iir)
-title('mA filtered iir')
-legend('mA','fir','iir')
+% plot(t,mA_filtered)
+% title('mA filtered')
+% plot(t,mA_filtered_iir)
+% title('mA filtered iir')
+plot(t_down,mA_down,'x', 'MarkerSize',20)
+title('mA downsampled')
+legend('mA','mA downsampled')
 
-subplot(1,2,2)
-plot(f,P1) 
-title('mA spectrum [dB]')
+figure
+plot(t_down,mA_down)
+title('mA downsampled')
+
+% subplot(1,2,2)
+% plot(f,P1) 
+% title('mA spectrum [dB]')
